9cc1d3881560ddec20be53908476f36b6076e5ef
Ben <54753989+R1D3R175@users.noreply.github.com>
2022-07-08 03:03:27 +0200

    Added some echoes

--- infrastructure/suricata/docker-setup.sh
+++ infrastructure/suricata/docker-setup.sh
@@ -128,29 +128,30 @@ then
     exit 0
 fi
 
-echo -e "${RED_BOLD}Creating ${INSTALL_DIR}/... ${RESET}"
+echo -e "${RED_BOLD}Creating ${INSTALL_DIR}/ ${RESET}"
 mkdir -p "${INSTALL_DIR}/" &> "$VERBOSE"
 
 if [ ! -d "${INSTALL_DIR}/" ];
 then
-    echo -e "${RED_BOLD}Failed to create ${INSTALL_DIR}/...${RESET}"
+    echo -e "${RED_BOLD}Failed to create ${INSTALL_DIR}/${RESET}"
     exit 0
 fi
 
 echo -e "${RED_BOLD}Creating sub-directories for Docker Volumes: ${RESET}\n\
-	${GREEN}- ${INSTALL_DIR}/etc/ ${RESET}\n\
-	${GREEN}- ${INSTALL_DIR}/lib/ ${RESET}\n\
-	${GREEN}- ${INSTALL_DIR}/log/ ${RESET}"
+	${GREEN}- ${INSTALL_DIR}/etc/${RESET}\n\
+	${GREEN}- ${INSTALL_DIR}/lib/${RESET}\n\
+	${GREEN}- ${INSTALL_DIR}/log/${RESET}"
 mkdir {"${INSTALL_DIR}/etc/","${INSTALL_DIR}/lib/","${INSTALL_DIR}/log/"} &> "$VERBOSE"
 
-echo -e "${RED_BOLD}Running Suricata container (detatch)...${RESET}"
+echo -e "${RED_BOLD}Running Suricata container with interface $1${RESET}"
 docker run --rm -it -d --net=host \
 --cap-add=net_admin --cap-add=net_raw --cap-add=sys_nice \
 -v "${INSTALL_DIR}/etc":/etc/suricata -v "${INSTALL_DIR}/lib":/var/lib/suricata -v "${INSTALL_DIR}/log":/var/log/suricata \
 --name=suricata jasonish/suricata:latest-amd64 -i "$1" &> "$VERBOSE"
 
-echo -e "${RED_BOLD}Fixing SIP, MQTT and RDP warnings.\n\
-Creating rules file ${INSTALL_DIR}/etc/ctf.rules${RESET}"
+echo -e "${RED_BOLD}Fixing SIP, MQTT and RDP warnings.${RESET}"
+echo -e "${RED_BOLD}Creating rules file ${INSTALL_DIR}/etc/ctf.rules
+and adding it to rules-file in ${INSTALL_DIR}/etc/suricata.yaml${RESET}"
 touch "${INSTALL_DIR}/etc/ctf.rules" &> "$VERBOSE"
 
 PYTHON_FIX='import re
@@ -173,8 +174,9 @@ with open(FILE_PATH, "w") as modified_file:
 
 python3 -c "${PYTHON_FIX}" &> "$VERBOSE"
 
-echo -e "${RED_BOLD}Updating Suricata${RESET}"
+echo -e "${RED_BOLD}Running suricata-update${RESET}"
 docker exec -it --user suricata suricata suricata-update -f &> "$VERBOSE"
+echo -e "${RED_BOLD}Running suricatasc -c reload-rules${RESET}"
 
 echo -e "${RED_BOLD}Re-attaching back to Suricata's container${RESET}"
 docker attach suricata
